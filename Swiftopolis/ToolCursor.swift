c1639c1363e27a9fdb2a96c4f3230505730972df
Christian Benincasa <chrisbenincasa@gmail.com>
2015-02-03 17:06:40 -0500

    Redo how ToolCursors are generated

--- Swiftopolis/ToolCursor.swift
+++ Swiftopolis/ToolCursor.swift
@@ -11,8 +11,45 @@ import Cocoa
 
 struct ToolCursorColors {
     static let BACKGROUND_OPACITY: CGFloat = 0.375
-    static let RESIDENTIAL_TOOL_COLOR = NSColor.greenColor()
-    static let COMMERCIAL_TOOL_COLOR = NSColor.blueColor()
+    static let BROWNISH_COLOR = NSColor(calibratedRed: 191, green: 121, blue: 0, alpha: 1)
+    static let GRAYISH_COLOR = NSColor(calibratedRed: 93, green: 93, blue: 93, alpha: 1)
+    
+    static func colorsForTool(tool: Tool) -> (NSColor, NSColor) {
+        switch tool {
+        case .Residential:
+            let color = NSColor.greenColor()
+            return (color.colorWithAlphaComponent(BACKGROUND_OPACITY), color)
+        case .Commercial:
+            let color = NSColor.blueColor()
+            return (color.colorWithAlphaComponent(BACKGROUND_OPACITY), color)
+        case .Industrial:
+            let color = NSColor.yellowColor()
+            return (color.colorWithAlphaComponent(BACKGROUND_OPACITY), color)
+        case .Bulldozer:
+            return (NSColor.clearColor(), BROWNISH_COLOR)
+        case .Wire:
+            return (NSColor.blackColor().colorWithAlphaComponent(BACKGROUND_OPACITY), NSColor.yellowColor())
+        case .Road:
+            return (NSColor.whiteColor().colorWithAlphaComponent(BACKGROUND_OPACITY), GRAYISH_COLOR)
+        case .Rail:
+            return (NSColor(calibratedRed: 127, green: 127, blue: 0, alpha: 1), GRAYISH_COLOR)
+        case .FireStation:
+            return (NSColor.greenColor().colorWithAlphaComponent(BACKGROUND_OPACITY), NSColor.redColor())
+        case .PoliceStation:
+            return (NSColor.greenColor().colorWithAlphaComponent(BACKGROUND_OPACITY), NSColor.blueColor())
+        case .Coal, .Nuclear:
+            return (GRAYISH_COLOR.colorWithAlphaComponent(BACKGROUND_OPACITY), NSColor.yellowColor())
+        case .Stadium:
+            return (GRAYISH_COLOR.colorWithAlphaComponent(BACKGROUND_OPACITY), NSColor.greenColor())
+        case .Seaport:
+            return (GRAYISH_COLOR.colorWithAlphaComponent(BACKGROUND_OPACITY), NSColor.blueColor())
+        case .Airport:
+            return (GRAYISH_COLOR.colorWithAlphaComponent(BACKGROUND_OPACITY), BROWNISH_COLOR)
+        case .Park:
+            return (NSColor.greenColor().colorWithAlphaComponent(BACKGROUND_OPACITY), BROWNISH_COLOR)
+        default: return (NSColor.clearColor(), NSColor.clearColor())
+        }
+    }
 }
 
 class ToolCursor {
@@ -26,21 +63,8 @@ class ToolCursor {
         self.fillColor = fillColor
     }
     
-    class func residentialTool(rect withRect: NSRect = NSRect.zeroRect) -> ToolCursor {
-        var background = ToolCursorColors.RESIDENTIAL_TOOL_COLOR.colorWithAlphaComponent(ToolCursorColors.BACKGROUND_OPACITY)
-        return ToolCursor(rect: withRect, borderColor: ToolCursorColors.RESIDENTIAL_TOOL_COLOR, fillColor: background)
-    }
-    
-    class func commercialTool(rect withRect: NSRect = NSRect.zeroRect) -> ToolCursor {
-        var background = ToolCursorColors.COMMERCIAL_TOOL_COLOR.colorWithAlphaComponent(ToolCursorColors.BACKGROUND_OPACITY)
-        return ToolCursor(rect: withRect, borderColor: ToolCursorColors.COMMERCIAL_TOOL_COLOR, fillColor: background)
-    }
-    
-    class func toolCursorForTool(tool: Tool, rect withRect: NSRect = NSRect.zeroRect) -> ToolCursor {
-        switch tool {
-        case .Residential: return residentialTool(rect: withRect)
-        case .Commercial: return commercialTool(rect: withRect)
-        default: fatalError("Unrecognized tool type")
-        }
+    convenience init(tool: Tool, withRect rect: NSRect) {
+        let (backgroundColor, borderColor) = ToolCursorColors.colorsForTool(tool)
+        self.init(rect: rect, borderColor: borderColor, fillColor: backgroundColor)
     }
 }
